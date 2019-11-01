f4d96844946d5c7c2da97fd0e487b17a0d601432
ben14 <544652471@qq.com>
2019-11-01 08:02:26 +0800

    feat: change gitignore.

---
+++ stories/search_bar.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+// import Test from "../package/search-bar/src/test";
+import SearchBar from "./package/search-bar/src";
+
+import "antd/dist/antd.css";
+
+export default {
+  title: "搜索条"
+};
+
+const schema = {
+  type: "object",
+  properties: {
+    name: {
+      type: "radio",
+      enum: ["1", "2", "3", "4"],
+      title: "name"
+    }
+  }
+};
+const onSearch = (v) => {
+  console.log(
+    "%c搜索关键词",
+    "background: #69c0ff; color: white; padding: 4px",
+    v
+  );
+};
+
+export const Demo = () => {
+  return <SearchBar schema={schema} onCaptureForm={onSearch} />;
+};
