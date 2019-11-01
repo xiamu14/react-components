c6a3a229339a762a9a244b5e91f43d03e116b54e
ben14 <544652471@qq.com>
2019-11-01 13:02:50 +0800

    feat: delete submodule.

---
+++ package/search-table/README.md
@@ -0,0 +1,31 @@
+# search-table
+
+> 搜索表格(基于 antd, search-bar)
+
+[![NPM](https://img.shields.io/npm/v/search-table.svg)](https://www.npmjs.com/package/search-table) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save search-table
+```
+
+## Usage
+
+```tsx
+import * as React from 'react'
+
+import MyComponent from 'search-table'
+
+class Example extends React.Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+MIT © [xiamu14](https://github.com/xiamu14)
---
+++ package/search-table/src/index.tsx
@@ -0,0 +1,70 @@
+/**
+ * @desc 带搜索功能的表格
+ */
+
+import React, { useState } from "react";
+import { Table } from "antd";
+import SearchBar from "@redchili/search-bar";
+
+import './index.scss';
+
+interface Props {
+  schema: Record<string, any>;
+  columns: Record<string, any>[];
+  dataSource: Record<string, any>[];
+  onChange: (page: number, searchKey: any) => void;
+  pagination?: Record<string, any>;
+  tableProps?: Record<string, any>;
+}
+
+export default function SearchTable(props: Props) {
+  const [curPage, setCurPage] = useState<number>(1);
+
+  const [searchKey, setSearchKey] = useState();
+
+  const { onChange } = props;
+
+  const onCaptureForm = (v: any) => {
+    setSearchKey(v);
+    setCurPage(1);
+    onChange(1, v);
+  };
+
+  const onSearchReset = () => {
+    setSearchKey(undefined);
+    setCurPage(1);
+    onChange(1, undefined);
+  };
+
+  const onGoPage = (page: number) => {
+    setCurPage(page);
+
+    onChange(page, searchKey);
+  };
+
+  const { schema, columns, dataSource, pagination, tableProps } = props;
+
+  const presetPagination = {
+    current: curPage,
+    onChange: onGoPage
+  };
+
+  return (
+    <div className="search_table--box">
+      <SearchBar
+        schema={schema}
+        onCaptureForm={onCaptureForm}
+        onSearchReset={onSearchReset}
+      />
+      <div className="h20" />
+      <Table
+        {...tableProps}
+        columns={columns}
+        dataSource={dataSource}
+        pagination={
+          pagination ? { ...pagination, ...presetPagination } : presetPagination
+        }
+      />
+    </div>
+  );
+}
