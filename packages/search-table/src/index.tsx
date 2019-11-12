cff532a2c5653e6e984cf36cfd348b7e5e87a7a6
ben14 <544652471@qq.com>
2019-11-12 14:27:19 +0800

    feat: add react-available-hooks.

--- packages/search-table/src/index.tsx
+++ packages/search-table/src/index.tsx
@@ -2,9 +2,10 @@
  * @desc 带搜索功能的表格
  */
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { Table } from "antd";
 import SearchBar from "@redchili/search-bar";
+import {useTableBodyHeight} from 'react-available-hooks';
 
 import "./index.scss";
 
@@ -42,21 +43,12 @@ export default function SearchTable(props: Props) {
     onChange(page, searchKey);
   };
 
-  const restScrollRef = useRef<any>(null);
+  const parentRef = useRef<any>(null);
   const searchRef = useRef<any>(null);
 
   /** @desc 表格固定高度 */
-  const [tableScrollHeight, setTableScrollHeight] = useState(0);
 
-  useEffect(() => {
-    if (restScrollRef.current && searchRef.current) {
-      const height =
-        restScrollRef.current.clientHeight -
-        searchRef.current.clientHeight -
-        120; // NOTE:减去底部分页 和表头
-      setTableScrollHeight(height);
-    }
-  }, [restScrollRef]);
+  const { tableBodyHeight } = useTableBodyHeight(parentRef)
 
   const { schema, columns, dataSource, pagination, tableProps } = props;
 
@@ -66,7 +58,7 @@ export default function SearchTable(props: Props) {
   };
 
   return (
-    <div className="search_table--box" ref={restScrollRef}>
+    <div className="search_table--box" ref={parentRef}>
       <div ref={searchRef}>
         <SearchBar
           schema={schema}
@@ -78,7 +70,7 @@ export default function SearchTable(props: Props) {
         {...tableProps}
         columns={columns}
         dataSource={dataSource}
-        scroll={{ y: tableScrollHeight }}
+        scroll={{ y: tableBodyHeight }}
         pagination={
           pagination ? { ...pagination, ...presetPagination } : presetPagination
         }
