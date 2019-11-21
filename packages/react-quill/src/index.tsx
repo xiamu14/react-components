518c963bbfb38676a419e32defa957e124fba788
ben14 <544652471@qq.com>
2019-11-21 17:23:34 +0800

    fix: fix asynchronous value on react-quill error.

--- packages/react-quill/src/index.tsx
+++ packages/react-quill/src/index.tsx
@@ -27,62 +27,62 @@ export default function ReactQuill(props: Props) {
   const quillBoxEl = useRef<any>(null);
   const inputEl = useRef<any>(null);
   const [medioType, setMedioType] = useState<"image" | "video">("image");
-
+  const [editor, setEditor] = useState();
   useEffect(() => {
-    VideoBlot.blotName = "cusVideo";
-    VideoBlot.tagName = "video";
-    Quill.register(VideoBlot);
+    if (!editor) {
+      setEditor(new Quill(quillBoxEl.current, props.options));
+    } else {
+      if (props.value) {
+        const delta = editor.clipboard.convert(props.value);
+        editor.setContents(delta);
+      } else {
+        VideoBlot.blotName = "cusVideo";
+        VideoBlot.tagName = "video";
+        Quill.register(VideoBlot);
+        editor.on(
+          "text-change",
+          _throttle(() => {
+            const el = document.querySelector(".ql-editor");
+            props.onChange && props.onChange(el ? el.innerHTML : "");
+          }, 1000)
+        );
 
-    const editor = new Quill(quillBoxEl.current, props.options);
+        const toolbar = editor.getModule("toolbar");
 
-    // NOTE: init quill
-    const { value } = props;
-    if (value) {
-      const delta = editor.clipboard.convert(value);
-      editor.setContents(delta);
-    }
+        toolbar.addHandler("image", () => {
+          setMedioType("image");
+          inputEl.current.click();
+        });
 
-    editor.on(
-      "text-change",
-      _throttle(() => {
-        const el = document.querySelector(".ql-editor");
-        props.onChange && props.onChange(el ? el.innerHTML : "");
-      }, 1000)
-    );
+        toolbar.addHandler("video", () => {
+          setMedioType("video");
+          inputEl.current.click();
+        });
 
-    const toolbar = editor.getModule("toolbar");
-
-    toolbar.addHandler("image", () => {
-      setMedioType("image");
-      inputEl.current.click();
-    });
-
-    toolbar.addHandler("video", () => {
-      setMedioType("video");
-      inputEl.current.click();
-    });
-
-    inputEl.current.addEventListener("change", async () => {
-      const files = inputEl.current.files;
-      const medioTypeCopy = inputEl.current.getAttribute("name");
-      const addImageRange = editor.getSelection()
-      const newRange = 0 + (addImageRange !== null ? addImageRange.index : 0);
-      if (files.length > 0 && props.medioRequest) {
-        const resFile = await props.medioRequest(files, medioTypeCopy);
-        if (medioTypeCopy === "image") {
-          editor.insertEmbed(newRange, "image", resFile.url);
-        } else {
-          editor.insertEmbed(newRange, "cusVideo", {
-            url: resFile.url,
-            controls: "controls",
-            width: "100%",
-            height: "100%"
-          });
-        }
+        inputEl.current.addEventListener("change", async () => {
+          const files = inputEl.current.files;
+          const medioTypeCopy = inputEl.current.getAttribute("name");
+          const addImageRange = editor.getSelection();
+          const newRange =
+            0 + (addImageRange !== null ? addImageRange.index : 0);
+          if (files.length > 0 && props.medioRequest) {
+            const resFile = await props.medioRequest(files, medioTypeCopy);
+            if (medioTypeCopy === "image") {
+              editor.insertEmbed(newRange, "image", resFile.url);
+            } else {
+              editor.insertEmbed(newRange, "cusVideo", {
+                url: resFile.url,
+                controls: "controls",
+                width: "100%",
+                height: "100%"
+              });
+            }
+          }
+          editor.setSelection(1 + newRange, 1);
+        });
       }
-      editor.setSelection(1 + newRange, 1);
-    });
-  }, []);
+    }
+  }, [props, editor]);
 
   const { width, height, minHeight } = props;
 
--- stories/react_quill.stories.tsx
+++ stories/react_quill.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactQuill, { ResFiles } from "../packages/react-quill/src";
 import "quill/dist/quill.snow.css";
 import "../packages/react-quill/dist/local.css";
@@ -24,8 +24,6 @@ const options = {
   theme: "snow"
 };
 
-const value = "<p>查看一下啊</p>";
-
 export const withReactQuill = () => {
   const cusRequest = async (
     res: any,
@@ -44,6 +42,12 @@ export const withReactQuill = () => {
           : "//static-beta.5facepay.com/information/2019_11_20/201911201407227ca165fd0c85aced8969bf90a6c8b0d5.mp4"
     };
   };
+
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    setValue('<p>这样子吗？<img src="//static-beta.5facepay.com/information/2019_11_21/20191121160931f7a7ff28d37889202ec4d7e3020d9198.png"></p><p>还好吧。应该</p><video src="//static-beta.5facepay.com/information/2019_11_21/2019112116094737eae21c9cd4e4a1a578ae95a4efa48f.mp4" controls="controls" width="100%" height="100%" webkit-playsinline="true" playsinline="true" x5-playsinline="true"></video><p><br></p>')
+  }, []);
 
   return (
     <div style={{ padding: "10px" }}>
