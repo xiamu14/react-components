d390982d2c3fd76dcbde2177f0e2f87bee06a40a
ben <544652471@qq.com>
2020-06-24 18:37:50 +0800

    feat: Add data and schema properties to describe the descriptions-pro component(增加 data 和 schem 两个属性来描述 descriptions-pro 组件)

--- packages/descriptions-pro/src/index.tsx
+++ packages/descriptions-pro/src/index.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Descriptions } from "antd";
 import { DescriptionsProps } from "antd/lib/descriptions";
+import { matcher } from 'data-matcher';
 import "./index.scss";
+import { ReactNode } from "react";
 
-export interface ItemType {
-  prefixCls?: string;
-  className?: string;
-  label: React.ReactNode;
+export interface Schema {
+  label: string;
+  dataIndex: string;
+  render?: (val: any) => ReactNode;
   span?: number;
   sort?: number;
-  content: React.ReactNode;
+  className?: string;
+  prefixCls?: string;
 }
 
+export interface Item {
+  [key: string]: string | number
+}
+
+// export interface ItemType {
+//   prefixCls?: string;
+//   className?: string;
+//   label: React.ReactNode;
+//   span?: number;
+//   sort?: number;
+//   content: React.ReactNode;
+// }
+
 export interface DescriptionsProProps extends DescriptionsProps {
-  data: ItemType[];
+  // data: ItemType[];
+  schema: Schema[];
+  data: Item;
   /** 文本居中显示(只有显示 border时 才有效果) */
   center?: boolean;
 }
@@ -22,13 +40,19 @@ const { Item } = Descriptions;
 
 export default function DescriptionsPro(props: DescriptionsProProps) {
 
-  const { data, center, ...other } = props;
+  const { schema, data, center, ...other } = props;
 
-  const sortedData = data.sort((a, b) => (b.sort || 0) - (a.sort || 0))
+  const middle = matcher(schema).addKeyFn("content", (val: Schema) => {
+    if (Object.prototype.hasOwnProperty.call(val, "render")) {
+      return (val.render as (val: any) => ReactNode)(data[val.dataIndex]);
+    }
+    return data[val.dataIndex];
+  }).remove(['dataIndex', 'render']);
+  const sortedData = (middle.data as Record<string, any>[]).sort((a: { sort?: number }, b: { sort?: number }) => (b.sort || 0) - (a.sort || 0))
 
   return (
     <div className={`${center ? 'text-align-center descriptions-pro' : "descriptions-pro"}  `}>
-      {data.length > 0 ? <Descriptions {...other}>
+      {Object.keys(data).length > 0 ? <Descriptions {...other}>
         {
           sortedData.map((item, index) => {
             const { content, ...otherItem } = item;
--- stories/descriptions_pro.stories.tsx
+++ stories/descriptions_pro.stories.tsx
@@ -1,40 +1,44 @@
 import React from "react";
-import DescriptionPro, { ItemType } from "../packages/descriptions-pro/src";
+import DescriptionPro, { Item, Schema } from "../packages/descriptions-pro/src";
 
 import "antd/dist/antd.css";
 
 export default {
   title: "描述组件 pro 版"
 };
-
-const data: ItemType[] = [
+const data: Item = {
+  name: "不对",
+  age: 12,
+  interest: "看书，电影，旅游，健身，创造",
+  picture: 'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+}
+const schema: Schema[] = [
   {
     label: "姓名",
-    content: "不对"
+    dataIndex: 'name'
   },
   {
     label: "年龄",
-    content: 12
+    dataIndex: "age"
   },
   {
     label: "爱好",
-    content: '看书，电影，旅游，健身，创造',
+    dataIndex: 'interest',
     sort: -1
-  }, 
-  {
-    label: "图片",
-    content: <img src="https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt='照片' width="40px" height="40px" />,
-    sort: -2,
   },
   {
-    label: "图片2",
-    content: <img src="" alt="" />
-  }
+    label: "图片",
+    render: (src: string) => <img src={src} alt='照片' width="40px" height="40px" />,
+    dataIndex: "picture",
+    sort: -2,
+  },
 ];
+
 
 export const DescriptionsProNoProps = () => {
   return (
     <DescriptionPro
+      schema={schema}
       data={data}
       center
       bordered
